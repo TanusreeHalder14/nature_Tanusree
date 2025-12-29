4781aab522e32bfa5aee283be74c5a6471e9de1f
TanusreeHalder14 <32800673+TanusreeHalder14@users.noreply.github.com>
2025-12-29 18:17:27 +0530

    path adde2

--- shortener/models.py
+++ shortener/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class ShortenedURL(models.Model):
-    original_url = models.URLField()
+    original_url = models.URLField(max_length=800, unique=True)
     short_url = models.CharField(max_length=8, unique=True)
     # expired_at = models.DateTimeField(auto_now_add=True)
